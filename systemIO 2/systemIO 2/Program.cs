9cf514b8bc152ed8398a51b2ac30b0263c79b5f2
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-03-29 11:44:32 +0200

    Add files via upload

---
+++ systemIO 2/systemIO 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace systemIO_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi svoje ime: ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if (File.Exists(datoteka))
+            {
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+
+                File.Copy(datoteka, @"backup\ime_"
+                    + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")
+                    + ".txt");
+
+                File.Delete(datoteka);
+            }
+
+            File.WriteAllText(datoteka, ime);
+
+            Console.ReadKey();
+            
+        }
+
+    }
+}
